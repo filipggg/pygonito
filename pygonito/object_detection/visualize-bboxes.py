ffb24de1f14aa801c1df31ab708a4d21ff814617
Filip Gralinski <filip.gralinski@applica.ai>
2022-03-03 08:21:39 +0100

    Add script for visualizing bboxes

---
+++ pygonito/object_detection/visualize-bboxes.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import cv2
+from bounding_box import bounding_box as bb
+import sys
+import re
+
+
+in_dir = sys.argv[1]
+out_dir = sys.argv[2]
+
+bbox_re = re.compile(r'^([^:]+):(\d+),(\d+),(\d+),(\d+)(:.*)?$')
+
+
+def parse_bbox(b):
+    m = bbox_re.match(b)
+    if m:
+        return [int(m.group(2)), int(m.group(3)),
+                int(m.group(4)), int(m.group(5)),
+                m.group(1)]
+    else:
+        raise f'WRONG BBOX {b}'
+
+
+def parse_bboxes(s):
+    return [parse_bbox(b) for b in s.split()]
+
+
+def process_file(in_file, bboxes, out_file):
+    image = cv2.imread(in_file)
+    for bbox in bboxes:
+        bb.add(image, bbox[0], bbox[1], bbox[2], bbox[3], bbox[4])
+
+    cv2.imwrite(out_file, image)
+
+
+for line in sys.stdin:
+    line = line.rstrip()
+    img_file, annotations = line.split('\t')
+    bboxes = parse_bboxes(annotations)
+    process_file(f'{in_dir}/{img_file}', bboxes, f'{out_dir}/{img_file}')
