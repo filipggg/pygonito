0e94b5c197171f2ece5d77b6b43cb79100785519
Filip Gralinski <filip.gralinski@applica.ai>
2022-03-07 21:27:21 +0100

    Add optional resizing

--- pygonito/object_detection/visualize-bboxes.py
+++ pygonito/object_detection/visualize-bboxes.py
@@ -4,15 +4,27 @@ import cv2
 from bounding_box import bounding_box as bb
 import sys
 import re
+import argparse
+import os
 
-if len(sys.argv) != 3:
-    print('Script for visualizing bboxes in Gonito challenges', file=sys.stderr)
-    print('For example, to visualize 10 first images from the train set:', file=sys.stderr)
-    print('   paste train/in.tsv train/expected.tsv| head -n 10 | ./visualize-bboxes.py images out', file=sys.stderr)
+parser = argparse.ArgumentParser(description='Visualize bounding boxes for the gold standard or an output for a Gonito object-detection challenge.')
+parser.add_argument('indir', metavar='IN_DIR', type=str,
+                    help='directory with input images')
+parser.add_argument('outdir', metavar='OUR_DIR', type=str,
+                    help='directory for output images (must exist)')
+parser.add_argument('--resize', type=int,
+                    help='optional resize factor (percent)')
+args = parser.parse_args()
+
+# For example, to visualize 10 first images from the train set:
+#    paste train/in.tsv train/expected.tsv| head -n 10 | ./visualize-bboxes.py images out
+
+in_dir = args.indir
+out_dir = args.outdir
+
+if not os.path.isdir(out_dir):
+    print(f'{out_dir} does not exist!', file=sys.stderr)
     exit(1)
-
-in_dir = sys.argv[1]
-out_dir = sys.argv[2]
 
 bbox_re = re.compile(r'^([^:]+):(\d+),(\d+),(\d+),(\d+)(:.*)?$')
 
@@ -33,8 +45,22 @@ def parse_bboxes(s):
 
 def process_file(in_file, bboxes, out_file):
     image = cv2.imread(in_file)
+
+    factor = 1.0
+
+    if args.resize is not None:
+        factor = args.resize / 100
+
+        width = int(image.shape[1] * factor)
+        height = int(image.shape[0] * factor)
+        dim = (width, height)
+
+        image = cv2.resize(image, dim)
+
     for bbox in bboxes:
-        bb.add(image, bbox[0], bbox[1], bbox[2], bbox[3], bbox[4])
+        bb.add(image,
+               int(bbox[0]*factor), int(bbox[1]*factor), int(bbox[2]*factor),
+               int(bbox[3]*factor), bbox[4])
 
     cv2.imwrite(out_file, image)
 
