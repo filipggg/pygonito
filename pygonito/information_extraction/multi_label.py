d9fcf02a5e519faaea77b0018306b2aa752c3141
Filip Gralinski <filip.gralinski@applica.ai>
2022-05-10 14:43:00 +0200

    Add more utility function for MultiLabel

--- pygonito/information_extraction/multi_label.py
+++ pygonito/information_extraction/multi_label.py
@@ -5,6 +5,8 @@ import regex as re
 key_val_pair_regexp = re.compile(r'([^\s=]+)=(\S+)')
 val_prob_regexp = re.compile(r'(.*):(\d+(?:\.\d*)?)$')
 
+def quote_value(val: str) -> str:
+    return val.replace(' ', '_')
 
 def unquote_value(val: str) -> str:
     return val.replace('_', ' ')
@@ -18,11 +20,21 @@ def parse_probs(val: str) -> Tuple[str, float]:
         return (val, 1.0)
 
 
+def clean_line(line: str) -> str:
+    return line.rstrip('\n')
+
+
 def parse_expected_line(line: str) -> Dict[str, str]:
     return {m.group(1): unquote_value(m.group(2))
-            for m in key_val_pair_regexp.finditer(line)}
+            for m in key_val_pair_regexp.finditer(clean_line(line))}
 
 
 def parse_output_line(line: str) -> Dict[str, Tuple[str, float]]:
     kvs = parse_expected_line(line)
     return {k: parse_probs(v) for (k, v) in kvs.items()}
+
+
+def format_data_point(data_point: Tuple[str, Tuple[str, float]]) -> str:
+    k, (v, p) = data_point
+    prob_suffix = ':' + str(p) if p < 1.0 else ''
+    return f'{k}={quote_value(v)}{prob_suffix}'
