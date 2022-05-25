b07a10c6e7fc5d19b9fadfd33f60569f0dbea119
Filip Gralinski <filip.gralinski@applica.ai>
2022-05-25 10:32:53 +0200

    Limit number of tokens

--- pygonito/ocr/ocr2bareocr/ocr2bareocr.py
+++ pygonito/ocr/ocr2bareocr/ocr2bareocr.py
@@ -4,18 +4,29 @@ from typing import List, Tuple
 import re
 import sys
 from PIL import Image
+import hashlib
 
 in_image_dir = sys.argv[1]
 out_image_dir = sys.argv[2]
 
 parse_ocr_re = re.compile(r'^([^:]+):(\d+),(\d+),(\d+),(\d+):(.*)$')
 
+page_limit = 20
+
+
+def unquote(t):
+    return t.replace('_', ' ')
+
+
+def clean(t):
+    return t.rstrip(' ')
+
 
 def parse_ocr_item(item: str) -> Tuple[str, Tuple[int, int, int, int], str]:
     m = parse_ocr_re.search(item)
 
     if m:
-        return (m.group(1), (int(m.group(2)), int(m.group(3)), int(m.group(4)), int(m.group(5))), m.group(6))
+        return (m.group(1), (int(m.group(2)), int(m.group(3)), int(m.group(4)), int(m.group(5))), unquote(m.group(6)))
     else:
         raise "???"
 
@@ -25,6 +36,21 @@ def parse_ocr(line: str) -> List[Tuple[str, Tuple[int, int, int, int], str]]:
         return []
 
     return [parse_ocr_item(item) for item in line.split()]
+
+
+def string_hash(s):
+    return hashlib.md5(s.encode('utf-8')).hexdigest()
+
+
+def item_hash(doc_id, pair):
+    ix, _ = pair
+    return string_hash(f'{doc_id}-ix')
+
+
+def new_tuple(t, ix, v):
+    l = list(t)
+    l[ix] = v
+    return tuple(l)
 
 
 def process_file(png_file, expected):
@@ -38,19 +64,28 @@ def process_file(png_file, expected):
     img = Image.open(f'{in_image_dir}/{png_file}')
     width, height = img.size
 
+    if page_limit is not None:
+        exp = [p[1] for p in sorted(
+            (sorted(enumerate(exp), key=lambda p: item_hash(doc_id, p)))[0:page_limit],
+            key=lambda p: p[0])]
+
     for _, area, content in exp:
+        image_ok = True
+
         if area[2] > width:
-            area[2] = width
+            image_ok = False
         if area[3] > height:
-            area[3] = height
+            image_ok = False
 
-        cropped_img = img.crop(area)
-        out_png = f'{out_image_dir}/{doc_id}-{bbox_id}.png'
-        cropped_img.save(out_png)
-        bbox_id += 1
+        if image_ok:
+            cropped_img = img.crop(area)
+            out_png = f'{out_image_dir}/{doc_id}-{bbox_id}.png'
+            cropped_img.save(out_png)
+            bbox_id += 1
 
-        print('\t'.join([out_png, content]))
-
+            print('\t'.join([out_png, clean(content)]))
+        else:
+            print(f'Something wrong with {doc_id}', file=sys.stderr)
 
 for line in sys.stdin:
     line = line.rstrip('\n')
